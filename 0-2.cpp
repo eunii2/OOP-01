53c9db9600f3a24141956e7a80b52e54246094fc
GaeunChoi <chb7562@gmail.com>
2023-04-14 20:34:23 +0900

    first commit

---
+++ 0-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "소프트웨어학부" << "\n";
+    cout << "22세" << "\n";
+    cout << "SW 개발자" << "\n";
+
+    return 0;
+}
---
+++ 0-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum =55;
+    cout << "1에서 10까지 더한 결과는" << sum << "입니다\n";
+
+    char c='#';
+    cout << c << 5.5 << '-' << "Hi";
+
+    return 0;
+}
